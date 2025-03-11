cd8fe767890fea052d5c1ce8efd1a764706d344b
SamAshray1 <sam.ashray1@gmail.com>
2025-03-11 13:16:44 +0530

    changed main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,71 +2,64 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+# Create an Internet Gateway for public access
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "eks-public-subnet"
+  }
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "eks-public-route-table"
+  }
+}
+
+# Associate Route Table with Public Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# EKS Cluster
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.public[*].id
+    subnet_ids = [aws_subnet.public.id]  # Use public subnet here
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_policy]
-}
-
-resource "aws_iam_role" "eks_role" {
-  name = "eksClusterRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_eks_node_group" "node_group" {
-  cluster_name  = aws_eks_cluster.eks.name
-  node_role_arn = aws_iam_role.node_role.arn
-  subnet_ids    = aws_subnet.public[*].id
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  depends_on = [aws_eks_cluster.eks]
-}
-
-resource "aws_iam_role" "node_role" {
-  name = "eksNodeGroupRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,7 @@ variable "aws_region" {
 variable "cluster_name" {
   default = "my-eks-cluster"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
